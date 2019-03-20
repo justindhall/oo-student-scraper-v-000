36d8151a9afb4eafe1becaa5161085f0bf87ae6e
Justin Hall <hall.justin.d@gmail.com>
2019-03-20 01:24:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student_array|
-      self.new(student)
+      self.new(student_hash)
   end
 
   def add_student_attributes(attributes_hash)
