207a84df404887533dc540272c5aaf49b9a3fa31
Justin Hall <hall.justin.d@gmail.com>
2019-03-20 01:14:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -48,7 +48,8 @@ class Scraper
       end
     end
     
-    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page
+    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if
     
     student
     
