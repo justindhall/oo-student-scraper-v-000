e27a9236df7a82ddef747529cb4e0e4791ea3fdb
Justin Hall <hall.justin.d@gmail.com>
2019-03-20 01:23:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
     student_hash.each do |key, value|
       self.send(("#{key}="), value )
     end
-    self << @@all
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
